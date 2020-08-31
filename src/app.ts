78844c1221b6cc231665b5f603173fba9ce014fa
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-30 23:02:12 -0300

    Create not found handler

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,12 @@ import { errors } from 'celebrate';
 const app = express();
 
 app.use(express.json());
+app.get('/*', (_, response) => {
+  response.status(404).json({
+    message: 'Resource not found',
+  });
+});
+
 app.use(errors());
 
 export default app;
