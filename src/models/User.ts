83b8f336e3a17c8e9652f9573eb6620284ddaf68
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-31 08:47:24 -0300

    Create user model

---
+++ src/models/User.ts
@@ -0,0 +1,17 @@
+import { model, Schema } from 'mongoose';
+
+const User = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default model('User', User);
