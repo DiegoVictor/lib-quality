fb4d59b87060cbdb4444e065bc614d79080c7581
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-30 23:01:01 -0300

    Configure celebrate

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config';
 import express from 'express';
+import { errors } from 'celebrate';
 const app = express();
 
 app.use(express.json());
+app.use(errors());
+
 export default app;
