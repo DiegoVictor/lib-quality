03fd83ae059dbf25f43bd5f67281a1343cbfd009
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-01 19:34:52 -0300

    Configure cors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import { errors } from 'celebrate';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import './database/mongodb';
 import routes from './routes';
@@ -9,6 +10,7 @@ import routes from './routes';
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use('/v1/', routes);
