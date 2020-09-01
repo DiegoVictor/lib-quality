941586f212ef371353051d25d2dff67e8e0fe548
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-01 19:34:35 -0300

    Configure helmet

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import { errors } from 'celebrate';
+import helmet from 'helmet';
 
 import './database/mongodb';
 import routes from './routes';
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 
 app.use('/v1/', routes);
