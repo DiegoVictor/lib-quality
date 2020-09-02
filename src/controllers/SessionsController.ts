9f7b6f979c5c885f83224c5616f3583a6683bd2c
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-01 22:10:06 -0300

    Move logic to service

--- src/controllers/SessionsController.ts
+++ src/controllers/SessionsController.ts
@@ -2,26 +2,13 @@ import jwt from 'jsonwebtoken';
 import { v4 } from 'uuid';
 import { Request, Response } from 'express';
 
-import UsersRepository from '../repositories/UsersRepository';
-
-const usersRepository = new UsersRepository();
+import { login } from '../services/UserService';
 
 class SessionsController {
   async store(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
-    const user = await usersRepository.findOneByEmail(email);
 
-    if (!user) {
-      return response.status(404).json({
-        message: 'User not found or not exists',
-      });
-    }
-
-    if (!usersRepository.comparePassword(password, user.password)) {
-      return response.status(400).json({
-        message: 'User and/or password not match',
-      });
-    }
+    const user = await login(email, password);
 
     return response.json({
       user: { _id: user._id },
