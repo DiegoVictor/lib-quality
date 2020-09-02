51a2a7a2c3b4e749d327e74801a59590549e1e68
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-01 22:09:55 -0300

    Update user creation

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,21 +1,12 @@
 import { Request, Response } from 'express';
 
-import UsersRepository from '../repositories/UsersRepository';
-
-const usersRepository = new UsersRepository();
+import { createUser } from '../services/UserService';
 
 class UsersController {
   async store(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
-    const user = await usersRepository.findOneByEmail(email);
 
-    if (user) {
-      return response.status(400).json({
-        message: 'Email already in use',
-      });
-    }
-
-    await usersRepository.create(email, password);
+    await createUser(email, password);
 
     return response.sendStatus(204);
   }
