a39ca5670a87e5e44be10387335c1b991b0bb985
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-01 22:12:31 -0300

    Create chart controller

---
+++ src/controllers/GitHubProjectAnalyticChartsController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import { badImplementation } from '@hapi/boom';
+
+import {
+  getRepositories,
+  getRepositoryIssuesStats,
+} from '../services/GithubService';
+
+interface CustomRequest {
+  query: {
+    repositories: string[];
+  };
+}
+
+class GitHubProjectAnalyticChartsController {
+  async show(
+    request: Request & CustomRequest,
+    response: Response,
+  ): Promise<Response> {
+    const { repositories } = request.query;
+
+    try {
+      const repos = await getRepositories(repositories);
+      const repositoryStats = await getRepositoryIssuesStats(
+        repos.map(({ full_name }) => full_name),
+      );
+
+      return response.json(repositoryStats);
+    } catch (err) {
+      console.log(err);
+
+      throw badImplementation(
+        'An error occured while getting issues statistics',
+      );
+    }
+  }
+}
+
+export default GitHubProjectAnalyticChartsController;
