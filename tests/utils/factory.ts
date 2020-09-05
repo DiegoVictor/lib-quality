f39d232733551ddb9e6b610ba6fe7d01f507ba9e
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-09-05 14:07:20 -0300

    Use model as constructor

--- tests/utils/factory.ts
+++ tests/utils/factory.ts
@@ -1,14 +1,11 @@
 import factory from 'factory-girl';
 import faker from 'faker';
+import User from '../../src/models/User';
 
-factory.define(
-  'User',
-  {},
-  {
-    email: faker.internet.email,
-    password: faker.internet.password,
-  },
-);
+factory.define('User', User, {
+  email: faker.internet.email,
+  password: faker.internet.password,
+});
 
 factory.define(
   'Issue',
