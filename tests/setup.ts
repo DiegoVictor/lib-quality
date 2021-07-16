27feb9b12c03e19613c4553a7d176d8fc25713b0
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-07-16 19:39:48 -0300

    Create server in memory

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,20 +1,16 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 
-const mongod = new MongoMemoryServer({
-  instance: {
-    ip: '127.0.0.1',
-    dbName: 'jest',
-    port: 27018,
-  },
-  autoStart: false,
-});
-
 module.exports = async () => {
-  await mongod.getConnectionString();
-
-  process.env.MONGO_HOST = '127.0.0.1';
-  process.env.MONGO_PORT = '27018';
-  process.env.MONGO_DB = 'jest';
-
+  const mongod = new MongoMemoryServer();
   (global as any).__MONGOD__ = mongod;
+
+  await mongod.start();
+
+  const config = mongod.instanceInfo;
+  if (config) {
+    const { port, dbName } = config;
+
+    process.env.MONGO_PORT = String(port);
+    process.env.MONGO_DB = dbName;
+  }
 };
