3c2fcc4dd18064d6b58e0db354afded386467ba7
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-07-17 18:38:37 -0300

    Set database host

--- tests/setup.ts
+++ tests/setup.ts
@@ -8,8 +8,9 @@ module.exports = async () => {
 
   const config = mongod.instanceInfo;
   if (config) {
-    const { port, dbName } = config;
+    const { port, dbName, ip } = config;
 
+    process.env.MONGO_HOST = ip;
     process.env.MONGO_PORT = String(port);
     process.env.MONGO_DB = dbName;
   }
